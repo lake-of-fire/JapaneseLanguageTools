fd98c8b83cfe2aa1174e44315c95b0c08558694e
Alex Ehlke <alex.ehlke@gmail.com>
2023-11-01 22:11:22 -0400

    wip

--- Sources/JapaneseLanguageTools/JapaneseLanguageTools.swift
+++ Sources/JapaneseLanguageTools/JapaneseLanguageTools.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public func containsCJKCharacters(text: String, includingKana: Bool = true, includingNumbers: Bool = false) -> Bool {
     return text.unicodeScalars.contains { scalar in
         var cjkRanges: [ClosedRange<UInt32>] = [
@@ -20,4 +22,39 @@ public func containsCJKCharacters(text: String, includingKana: Bool = true, incl
         }
         return cjkRanges.contains { $0.contains(scalar.value) }
     }
+}
+
+private let rendakuTransforms = [
+    "か" : ["が"],
+    "き" : ["ぎ"],
+    "く" : ["ぐ"],
+    "け" : ["げ"],
+    "こ" : ["ご"],
+    "さ" : ["ざ"],
+    "し" : ["じ"],
+    "す" : ["ず"],
+    "せ" : ["ぜ"],
+    "そ" : ["ぞ"],
+    "た" : ["だ"],
+    "ち" : ["ぢ", "じ"],
+    "つ" : ["づ", "ず"],
+    "て" : ["で"],
+    "と" : ["ど"],
+    "は" : ["ば", "ぱ"],
+    "ひ" : ["び", "ぴ"],
+    "ふ" : ["ぶ", "ぷ"],
+    "へ" : ["べ", "ぺ"],
+    "ほ" : ["ぼ", "ぽ"]
+]
+
+/// Requires preceding character to be kana, kanji or numeric.
+public func rendakuVariations(wordKana: String, accumulatedOutputText: String) -> [String] {
+    guard
+        let lastAccumulatedCharacter = accumulatedOutputText.last,
+        containsCJKCharacters(text: String(lastAccumulatedCharacter), includingKana: true, includingNumbers: true)
+            || (String(lastAccumulatedCharacter).rangeOfCharacter(from: CharacterSet.decimalDigits) != nil),
+        let firstCharacter = wordKana.first,
+        let transforms = rendakuTransforms[String(firstCharacter)]
+    else { return [] }
+    return transforms.map { wordKana.replacingCharacters(in: ...$0.utf16.startIndex, with: $0) }
 }
