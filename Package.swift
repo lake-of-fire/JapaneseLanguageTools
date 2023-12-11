85c0909b3f3f7da7c0005f6ff498a3a6676ee559
Alex Ehlke <alex.ehlke@gmail.com>
2023-12-11 15:38:48 -0500

    wip

--- Package.swift
+++ Package.swift
@@ -12,13 +12,16 @@ let package = Package(
             targets: ["JapaneseLanguageTools"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/lake-of-fire/RealmBinary.git", branch: "main"),
+//        .package(url: "https://github.com/lake-of-fire/RealmBinary.git", branch: "main"),
+        .product(name: "RealmSwift", package: "realm-swift"),
     ],
     targets: [
         .target(
             name: "JapaneseLanguageTools",
             dependencies: [
-                .product(name: "RealmSwift", package: "RealmBinary"),
+//                .product(name: "RealmSwift", package: "RealmBinary"),
+                .product(name: "Realm", package: "realm-swift"),
+                .product(name: "RealmSwift", package: "realm-swift"),
             ],
             resources: [
                 .copy("Resources/tofugu-audio-index.realm"),
